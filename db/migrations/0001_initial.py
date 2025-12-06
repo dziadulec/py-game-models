c9e45f597142187ee37e762174b980c639829140
Marek Drządzewski <dzadzumaster@gmail.com>
2025-12-06 16:25:54 +0100

    update migration file

--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -44,7 +44,7 @@ class Migration(migrations.Migration):
             name='Skill',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, unique=True)),
+                ('name', models.CharField(max_length=255)),
                 ('bonus', models.CharField(max_length=255, unique=True)),
                 ('race', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='skills', to='db.race')),
             ],
